fda6bdb845d0f807811292bf33bf0dfc493c2c42
Maniramzanpoor <maniramznpoor1386@gmail.com>
2021-11-26 11:53:06 +0330

    first commit

---
+++ version.ino
@@ -0,0 +1,38 @@
+//sensor pin
+//left-sensor
+int s1;
+int s2;
+int s3;
+//center-sensor
+int s4;
+int s5;
+int s6;
+//right-sensor
+int s7;
+int s8;
+//motor pin
+//righ-motor
+int m1 .1;
+int m1 .2;
+//left-motor
+int m2 .1;
+int m2 .2;
+void setup()
+{
+    // set-pin-mode
+    pinMode(s1, INPUT);
+    pinMode(s2, INPUT);
+    pinMode(s3, INPUT);
+    pinMode(s4, INPUT);
+    pinMode(s5, INPUT);
+    pinMode(s6, INPUT);
+    pinMode(s7, INPUT);
+    pinMode(m1 .1, OUTPUT);
+    pinMode(m1 .2, OUTPUT);
+    pinMode(m2 .1, OUTPUT);
+    pinMode(m2 .2, OUTPUT);
+}
+void loop()
+{
+    //made if or loop
+}
